1d5d1b117022ba0aef5a526911d12b5d5982f287
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-21 14:10:49 -0500

    Add: New Layout to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,26 @@ import Layout from '@/components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <section className='flex flex-col justify-center'>
-        <h1 className='text-3xl mb-2 text-center'>Next Trivia</h1>
-        <div className='flex flex-col gap-2 '>
-          <p className='p-2 bg-slate-200 text-center'>
+      <section className='flex flex-col justify-center md:max-w-screen-md md:mx-auto'>
+        <div className='flex flex-col gap-4 p-4'>
+          <p className='p-2 md:text-lg text-sm bg-slate-100 text-center rounded-md shadow-md'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
             doloribus quia quidem aspernatur laudantium fugit quod autem fugiat
             asperiores perferendis.
           </p>
           <ul className='flex flex-col gap-2'>
-            <li className='bg-slate-200 p-2'>Opcion 1</li>
-            <li className='bg-slate-200 p-2'>Opcion 2</li>
-            <li className='bg-slate-200 p-2'>Opcion 3</li>
-            <li className='bg-slate-200 p-2'>Opcion 4</li>
+            <li className='bg-slate-100 p-2 rounded-md border cursor-pointer'>
+              Opcion 1
+            </li>
+            <li className='bg-slate-100 p-2 rounded-md border cursor-pointer'>
+              Opcion 2
+            </li>
+            <li className='bg-slate-100 p-2 rounded-md border cursor-pointer'>
+              Opcion 3
+            </li>
+            <li className='bg-slate-100 p-2 rounded-md border cursor-pointer'>
+              Opcion 4
+            </li>
           </ul>
         </div>
       </section>
